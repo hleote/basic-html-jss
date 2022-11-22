a711877bfa9dd8be77bb835674831c06dd029c53
Hugo Leote <hleote@gmail.com>
2022-11-22 15:14:01 +0100

    Implement filters in a custom, readable way

--- week2/main-exercise/index.js
+++ week2/main-exercise/index.js
@@ -1,29 +1,25 @@
 /**
  * Get random integer between two numbers, found here: https://stackoverflow.com/a/7228322
- * @param {integer} min - The min number
- * @param {integer} max - The max number
+ * @param {number} min - The min number
+ * @param {number} max - The max number
  * @returns {Number} Random number between min and max
  */
 const randomIntFromInterval = (min, max) =>
     Math.floor(Math.random() * (max - min + 1) + min);
 
-const filterListings = (listToFilter = [], filterType = {}) =>
-    listToFilter.filter(item => item.type === filterType.type)
-
-
-const renderListings = (listings, filterType) => {
-    let filteredListing = listings;
-    if (filterType) {
-        filteredListing = filterListings(filteredListing, { type: filterType });
-    }
+/**
+ * Render the listings
+ * @param {array} listings - array of listings
+ * @returns {void}
+ */
+const addListingsToHTML = (listings) => {
     const listingsElement = document.querySelector('#listings');
     listingsElement.innerHTML = '';
-
-    if (filteredListing.length === 0) {
+    if (listings.length === 0) {
         listingsElement.innerHTML = '<h1>No listings found</h1>';
         return;
     }
-    filteredListing.forEach(listing => {
+    listings.forEach(listing => {
         listingsElement.innerHTML += `
             <li>
                 <h2>${listing.type}</h2>
@@ -37,8 +33,77 @@ const renderListings = (listings, filterType) => {
 }
 
 /**
+ * Function to render the listings
+ * @param {array} listings - array of filtered listings
+ * @param {string} filterType - the string with the type to filter on
+ * @returns {void}
+ */
+const renderFilteredListByType = (listings, filterType) => {
+    const filteredListing = listings.filter(item => item.type === filterType)
+    addListingsToHTML(filteredListing);
+}
+
+/**
+ * Function to render the listings
+ * @param {array} listings - array of filtered listings
+ * @param {string} facilityType - a string that contains the facility to filter on
+ * @returns {void}
+ */
+const renderFilteredListByFacilities = (listings, facilityType) => {
+    let filteredListing = [];
+    listings.forEach(listing => {
+        if (listing.facilities.includes(facilityType)) {
+            filteredListing.push(listing);
+        }
+    })
+    addListingsToHTML(filteredListing);
+}
+
+/**
+ * Function to render the listings by price
+ * @param {array} listings - array of filtered listings
+ * @param {string} price - price to filter on
+ * @returns {void}
+ */
+const renderFilteredListByPrice = (listings, price) => {
+    const biggerOrSmaller = price.substring(0, 1);
+    const priceNumber = parseInt(price.substring(1));
+    const filteredListing = listings.filter(listing => biggerOrSmaller === '-' ?
+        listing.price < priceNumber :
+        listing.price > priceNumber
+    );
+    addListingsToHTML(filteredListing);
+}
+
+/**
+ * Function to render the listings by size
+ * @param {array} listings - array of filtered listings
+ * @param {string} size - size to filter on
+ * @returns {void}
+ */
+const renderFilteredListBySize = (listings, size) => {
+    const biggerOrSmaller = size.substring(0, 1);
+    const sizeNum = parseInt(size.substring(1));
+    const filteredListing = listings.filter(listing => biggerOrSmaller === '-' ?
+        listing.size < sizeNum :
+        listing.size > sizeNum
+    );
+    addListingsToHTML(filteredListing);
+}
+
+/**
+ * Function to render the listings by listings with garden
+ * @param {array} listings - array of filtered listings
+ * @returns {void}
+ */
+const renderFilteredListByHasGarden = (listings) => {
+    const filteredListing = listings.filter(listing => listing.hasGarden);
+    addListingsToHTML(filteredListing);
+}
+
+/**
  * Get an array with listing objects with random color and speed
- * @param {integer} numberOfListings - The number of listings
+ * @param {number} numberOfListings - The number of listings
  * @returns {array} Array containing the listing objects
  */
 const generateListings = (numberOfListings) => {
@@ -93,7 +158,7 @@ const generateListings = (numberOfListings) => {
     // Listings with parking
     const listingsWithParking = listings.filter(listing => listing.facilities.includes('Parkering'))
     // console.log('listingsWithParking ', listingsWithParking)
-    renderListings(listings)
+    addListingsToHTML(listings)
     return listings;
 }
 
@@ -103,15 +168,37 @@ const listings = generateListings(5);
 const selectedTypeFilter = document.querySelector('#type-filter');
 selectedTypeFilter.addEventListener('change', (event) => {
     const filterType = event.target.value;
-    renderListings(listings, filterType);
+    renderFilteredListByType(listings, filterType);
+});
+
+const selectedFacilitiesFilter = document.querySelector('#facilities-filter');
+selectedFacilitiesFilter.addEventListener('change', (event) => {
+    const facilityType = event.target.value;
+    renderFilteredListByFacilities(listings, facilityType);
 });
 
 
-// TODO: Aditional homework - make it work for the price and facilities filters
-const selectedFacilitiesFilter = document.querySelector('#facilities-filter');
 const selectedPriceFilter = document.querySelector('#price-filter');
+selectedPriceFilter.addEventListener('change', (event) => {
+    const price = event.target.value;
+    renderFilteredListByPrice(listings, price);
+});
 
 
+const gardenFilter = document.querySelector('#garden-filter');
+gardenFilter.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        renderFilteredListByHasGarden(listings);
+        return;
+    }
+    addListingsToHTML(listings)
+});
+
+const sizeFilter = document.querySelector('#size-filter');
+sizeFilter.addEventListener('change', (event) => {
+    const size = event.target.value;
+    renderFilteredListBySize(listings, size);
+});
 
 
 // event listener for generating 5 new listings and removing all filters
