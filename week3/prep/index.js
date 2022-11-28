f0fa232bfe15b8f430b28d782b36b315eed494f9
Hugo Leote <hleote@gmail.com>
2022-11-28 09:06:52 +0100

    Calculate average of cursor coordenates every 20 seconds

--- week3/prep/index.js
+++ week3/prep/index.js
@@ -1,5 +1,5 @@
 
-// Counter exercise ------------------------------------------------------
+// Click counter exercise ------------------------------------------------------
 let counter = -1;
 const increment = function() {
     counter += 1;
@@ -12,22 +12,44 @@ initButton.addEventListener('click', increment);
 const incrementButton = document.querySelector('#increment');
 incrementButton.addEventListener('click', increment);
 
-
-// Delay logging exercise ------------------------------------------------------
+// Delay clicker logging exercise ------------------------------------------------------
 const delayedLog = function() {
-    document.getElementById('delayedGreet').innerHTML = 'This text was delayed by 3 seconds';
+    document.getElementById('delayed-greet').innerHTML = 'This text was delayed by 3 seconds';
 }
 const delayedButton = document.querySelector('#delay');
 delayedButton.addEventListener('click', () => {
     setTimeout(delayedLog, 3000);
 });
 
-// DOM loading exercise ------------------------------------------------------
-
+// Page onload exercise ------------------------------------------------------
 const callback = () => console.log('DOM fully loaded and parsed')
 
-addEventListener("load", (event) => callback());
+addEventListener("load", callback);
 
 onload = (event) => callback();
+
+//Mouse position & Mouse position online tool
+
+let scheduled = false;
+const mouseMoveCallback = (e) => {
+    const mouseMoveElement = document.querySelector("#mouse-move");
+    const mouseMovementXArray = [];
+    const mouseMovementYArray = [];
+    mouseMovementXArray.push(e.clientX);
+    mouseMovementYArray.push(e.clientY);
+    mouseMoveElement.innerHTML = `X: ${e.clientX}, Y: ${e.clientY}`;
+    if (!scheduled) {
+        scheduled = true;
+        setTimeout(() => {
+            const averageMouseMoveElement = document.querySelector("#average-mouse-move");
+            averageMouseMoveElement.innerHTML =
+                `Average X: ${mouseMovementXArray.reduce((a, b) => a + b, 0) / mouseMovementXArray.length}
+                        Average Y: ${mouseMovementYArray.reduce((a, b) => a + b, 0) / mouseMovementYArray.length}`;
+            scheduled = false;
+        }, 5000);
+    }
+}
+
+addEventListener("mousemove", mouseMoveCallback);
 
 
